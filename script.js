15ecf5d103e301db54a579f6bb8cf9374278cea1
Julian Danner <28jdanner@cpsd.us>
2026-06-17 12:42:31 -0400

    it works now

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const container = document.querySelector('#container');
 const button = document.querySelector('button');
+
 function makeGrid(size) {
     container.innerHTML = '';
-    for (let i=0; i < size * size; i++) {
-        const square = document.makeElement('div');
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
         square.style.flex = `1 0 calc(100% / ${size})`;
         square.style.height = `calc(100% / ${size})`;
-        sqaure.addEventListener('mouseover', () => {
+        square.addEventListener('mouseover', () => {
             square.style.backgroundColor = 'black';
         });
+        container.appendChild(square);
     }
 }
+
 button.addEventListener('click', () => {
-    let size = parseInt(prompt('put the dims (short for dimensions) that it should have:'));
+    let size = parseInt(prompt('Enter the dimensions you want (max 100):'));
     if (size > 0 && size <= 100) {
         makeGrid(size);
     }
 });
+
 makeGrid(16);
